c7e6ae45c6a077e744bad696e054452aabe15a17
Marta <Marta@Marta>
2017-03-15 17:58:25 +0000

    teste commit 2

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -27,9 +27,7 @@ import mt.filter.AnalyticsFilter;
  *
  */
 public class MicroServer implements MicroTraderServer {
-	//teste de commit
-	//primeiro trabalho ESII
-		
+	
 	public static void main(String[] args) {
 		ServerComm serverComm = new AnalyticsFilter(new ServerCommImpl());
 		MicroTraderServer server = new MicroServer();
