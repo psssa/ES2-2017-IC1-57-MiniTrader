8149f992add94f21ea3027108e38800c0ab46c5c
Ana Fernandes <ambfs@iscte-iul.pt>
2017-05-16 14:16:53 +0100

    JUnit US, AS and EU

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -23,7 +23,7 @@ import mt.filter.AnalyticsFilter;
  * MicroTraderServer implementation. This class should be responsible
  * to do the business logic of stock transactions between buyers and sellers.
  * 
- * @author Group 78
+ * @author Group 57
  *
  */
 public class MicroServer implements MicroTraderServer { //yjggg
